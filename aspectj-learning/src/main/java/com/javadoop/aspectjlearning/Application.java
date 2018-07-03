3940412ac010bd0f78dd91fe9bdf4865e2811c00
hongjiev <hongjie.v@gmail.com>
2018-07-03 10:51:50 +0800

    update

--- aspectj-learning/src/main/java/com/javadoop/aspectjlearning/Application.java
+++ aspectj-learning/src/main/java/com/javadoop/aspectjlearning/Application.java
@@ -6,18 +6,19 @@ import com.javadoop.aspectjlearning.share.model.User;
 
 public class Application {
 
+    /**
+     * 在测试时，请先修改 pom.xml 文件中的内容，放开相应的注释
+     * 不要用 IDE 跑，用命令行：
+     *      1. mvn clean package
+     *      2. java -jar target/aspectj-learning-1.0-jar-with-dependencies.jar
+     * 碰到你解决不了的问题，可以尝试到 https://www.javadoop.com/post/aspectj 留言
+     */
     public static void main(String[] args) {
-
-        /*
-         * 在测试时，请先修改 pom.xml 文件中的内容，放开相应的注释
-         * 不要用 IDE 跑，用命令行：
-         *     1. mvn clean package
-         *     2. java -jar target/aspectj-learning-1.0-jar-with-dependencies.jar
-         */
 
 //        testCompileTime();
 //        testPostCompile();
 //        testLoadTime();
+        
     }
 
 
